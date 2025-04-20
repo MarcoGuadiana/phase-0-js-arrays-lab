a7b967f7d25ff620ad35fbd189ed61fffe92f774
Marco Guadiana <marcoguadiana01@gmail.com>
2025-04-20 17:30:08 -0500

    5:29pm 4/20

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,40 @@
 // Write your code here
 
+const products = ["Laptop", "Phone" , "Headphones" , "Monitor"  ]
+
+
+
+logFirstProduct = products[0];
+
+
+
+function updateProductName(numPosition, newName)  {
+  for (let i = 0; i < products.length; i++) {
+    if (i == numPosition) {
+      products[i] = newName;
+      return newName;
+    }
+  }
+}
+updateProductName(1, "Smartphone");
+console.log(products);
+
+function  removeLastProduct(){
+  products.pop();
+
+}
+
+removeLastProduct();
+
+console.log(products);
+
+function addProduct(addItem){
+  products.push(addItem);
+}
+
+addProduct("Tablet");
+
+console.log(products[products.length -1]);
 
 
 // Export the necessary parts for testing
